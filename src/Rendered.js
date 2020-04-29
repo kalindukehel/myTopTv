fdf88c6bd26d0e9fc4a78476926e8c8766279c25
Kalindu <59674165+kalindu-k@users.noreply.github.com>
2020-04-29 19:06:24 -0400

    Optimize rendered image quality

--- src/Rendered.js
+++ src/Rendered.js
@@ -4,7 +4,8 @@ class Rendered extends React.Component{
    constructor(){
         super()
         this.state = {
-            src:""
+            src:"",
+            style:{}
         }
         this.imageLoaded = this.imageLoaded.bind(this)
         this.runThis = this.runThis.bind(this)
@@ -24,15 +25,16 @@ class Rendered extends React.Component{
     imageLoaded(e){
         /* create png from canvas */
         var c = document.createElement("canvas")
-        c.width = "450";
-        c.height = "286"
+        c.width = "675";
+        c.height = "432.09"
         var ctx = c.getContext("2d")
-        ctx.drawImage(e,0,0)
+        ctx.drawImage(e,0,0,450,288.06,0,0,675,432.09)
         var renderedPng =document.createElement("img")
         renderedPng.src = c.toDataURL('image/jpeg',1.0);
         renderedPng.onload = this.deleteNode()
         this.setState({
-            src:renderedPng.src
+            src:renderedPng.src,
+            style:{width:"450px",height:"288.06px"}
         })
     }
 
@@ -51,7 +53,7 @@ class Rendered extends React.Component{
     render(){
         return(
             <div>
-                <img id="rendered" alt="" src={this.state.src} />
+                <img style={this.state.style} id="rendered" alt="" src={this.state.src} />
                 <img id="thissvg" alt="" src={this.props.location.url} />
             </div>
         )
